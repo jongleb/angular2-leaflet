50ee673df730a7bbee36c59779074fca73dea6de
Gleb <jongleb@yandex.ru>
2017-01-11 16:09:41 +0300

    initial by angular2-cli

---
+++ src/components/leaflet-map.ts
@@ -0,0 +1,21 @@
+import {Component, ElementRef, EventEmitter, OnChanges, OnDestroy, OnInit, SimpleChange} from '@angular/core';
+
+@Component({
+  selector: 'jg-leaflet-map',
+  template: ``
+})
+export class LeafletMap implements OnChanges, OnInit, OnDestroy{
+
+  ngOnChanges() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
